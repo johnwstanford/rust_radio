a125c3a6b24cd7d14317ab9dae25a400fa6a2e11
John Stanford <johnwstanford@gmail.com>
2019-09-21 23:14:21 -0500

    Finished implementing subframe 5

--- src/gnss/telemetry_decode/gps/l1_ca_subframe.rs
+++ src/gnss/telemetry_decode/gps/l1_ca_subframe.rs
@@ -19,7 +19,23 @@ pub enum Subframe {
 	Subframe2{common:CommonFields, iode:u8, crs:f64, dn:f64, m0:f64, cuc:f64, e:f64, cus:f64, sqrt_a:f64, t_oe:f64, fit_interval:bool, aodo:u8 },
 	Subframe3{common:CommonFields, cic:f64, omega0:f64, cis:f64, i0:f64, crc:f64, omega:f64, omega_dot:f64, iode:u8, idot:f64},
 	Subframe4{common:CommonFields, data_id:u8, sv_id:u8},
-	Subframe5{common:CommonFields, data_id:u8, sv_id:u8},
+	Subframe5{common:CommonFields, data_id:u8, sv_id:u8, page:Subframe5},
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Subframe4 {
+	AlmanacData,
+	NavigationMessageCorrectionTable,
+	SpecialMessages,
+	IonosphereAndUTC,
+	AntispoofAndHealth,
+	Reserved,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Subframe5 {
+	AlmanacData{e:f64, t_oa:u32, delta_i:f64, omega_dot:f64, sv_health:u8, sqrt_a:f64, omega0:f64, omega:f64, m0:f64, af0:f64, af1:f64},
+	Page25{t_oa:u32, WN_a:u8, sv_health:[u8; 24]},
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -89,7 +105,33 @@ pub fn decode(bits:[bool; 240], start_sample_idx:usize) -> Result<Subframe, DigS
 		5 => {
 			let data_id:u8 = utils::bool_slice_to_u8(&bits[48..50]);
 			let sv_id:u8   = utils::bool_slice_to_u8(&bits[50..56]);
-			Ok(Subframe::Subframe5{ common, data_id, sv_id })
+			let page:Subframe5 = match sv_id {
+				1..=24 => {
+					let e:f64         = (utils::bool_slice_to_u16(&bits[ 56..72 ]) as f64) * (2.0_f64).powi(-21);
+					let t_oa:u32      =  utils::bool_slice_to_u32(&bits[ 72..80 ]) * 2_u32.pow(12);
+					let delta_i:f64   = (utils::bool_slice_to_i16(&bits[ 80..96 ]) as f64) * (2.0_f64).powi(-19);
+					let omega_dot:f64 = (utils::bool_slice_to_i16(&bits[ 96..112]) as f64) * (2.0_f64).powi(-38);
+					let sv_health:u8  =  utils::bool_slice_to_u8( &bits[112..120]);
+					let sqrt_a:f64    = (utils::bool_slice_to_u32(&bits[120..144]) as f64) * (2.0_f64).powi(-11);
+					let omega0:f64    = (utils::bool_slice_to_i32(&bits[144..168]) as f64) * (2.0_f64).powi(-23);
+					let omega:f64     = (utils::bool_slice_to_i32(&bits[168..192]) as f64) * (2.0_f64).powi(-23);
+					let m0:f64        = (utils::bool_slice_to_i32(&bits[192..216]) as f64) * (2.0_f64).powi(-23);
+					let af0:f64       = (utils::bool_slice_to_i16(&[&bits[216..224], &bits[235..238]].concat()) as f64) * (2.0_f64).powi(-20);
+					let af1:f64       = (utils::bool_slice_to_i32(&bits[224..235]) as f64) * (2.0_f64).powi(-18);
+					Subframe5::AlmanacData{e, t_oa, delta_i, omega_dot, sv_health, sqrt_a, omega0, omega, m0, af0, af1}
+				},
+				25 => {
+					let t_oa:u32 = utils::bool_slice_to_u32(&bits[56..64]) * 2_u32.pow(12);
+					let WN_a:u8  = utils::bool_slice_to_u8(&bits[64..72]);
+					let mut sv_health:[u8; 24] = [0; 24];
+					for i in 0..24 {
+						sv_health[i] = utils::bool_slice_to_u8(&bits[(72+(i*6))..(78+(i*6))]);
+					}
+					Subframe5::Page25{t_oa, WN_a, sv_health}
+				},
+				_ => return Err(DigSigProcErr::InvalidTelemetryData),
+			};
+			Ok(Subframe::Subframe5{ common, data_id, sv_id, page })
 		},
 		_ => Err(DigSigProcErr::InvalidTelemetryData),
 	}

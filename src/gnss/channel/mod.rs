685da0d0fd4b1e758c4e2ed990fe12fff8b3cf24
John Stanford <johnwstanford@gmail.com>
2019-09-26 23:12:07 -0500

    A few tweaks to the channel module

--- src/gnss/channel/mod.rs
+++ src/gnss/channel/mod.rs
@@ -11,6 +11,9 @@ use ::utils;
 
 pub const DEFAULT_PLL_BW_HZ:f64 = 40.0;
 pub const DEFAULT_DLL_BW_HZ:f64 = 4.0;
+pub const DEFAULT_DOPPLER_STEP_HZ:usize = 50;
+pub const DEFAULT_DOPPLER_MAX_HZ:i16 = 10000;
+pub const DEFAULT_TEST_STAT_THRESHOLD:f64 = 0.008;
 
 type Sample = (Complex<f64>, usize);
 
@@ -31,15 +34,19 @@ pub enum ChannelResult {
 pub struct Channel {
 	pub prn:usize,
 	pub fs:f64,
-	pub state:ChannelState,
-	pub acq: acquisition::Acquisition,
+	state:ChannelState,
+	acq: acquisition::Acquisition,
 	trk: tracking::Tracking,
 	tlm: telemetry_decode::gps::TelemetryDecoder,
+	last_acq_doppler:i16,
+	last_acq_test_stat:f64,
 }
 
 impl Channel {
 
 	pub fn carrier_freq_hz(&self) -> f64 { self.trk.carrier_freq_hz() }
+	pub fn last_acq_doppler(&self) -> i16 { self.last_acq_doppler }
+	pub fn last_acq_test_stat(&self) -> f64 { self.last_acq_test_stat }
 
 	pub fn initialize(&mut self, acq_freq:f64, code_phase:usize) {
 		self.state = match code_phase {
@@ -54,6 +61,8 @@ impl Channel {
 		ChannelState::Acquisition => {
 			if let Some(r) = self.acq.apply(s.0) {
 				self.initialize(r.doppler_hz as f64, r.code_phase);
+				self.last_acq_doppler = r.doppler_hz;
+				self.last_acq_test_stat = r.test_statistic;
 				ChannelResult::Acquisition{ doppler_hz: r.doppler_hz, test_stat: r.test_statistic }
 			}
 			else { ChannelResult::NotReady("Waiting on acquisition") }
@@ -92,12 +101,14 @@ impl Channel {
 
 }
 
-pub fn new_default_channel(prn:usize, fs:f64, acq_freq:f64) -> Channel {
+pub fn new_default_channel(prn:usize, fs:f64, acq_freq:f64) -> Channel { new_channel(prn, fs, acq_freq, DEFAULT_TEST_STAT_THRESHOLD) }
+
+pub fn new_channel(prn:usize, fs:f64, acq_freq:f64, test_stat:f64) -> Channel {
 	let state = ChannelState::Acquisition;
 	let symbol:Vec<i8> = l1_ca_signal::prn_int_sampled(prn, fs);
-	let acq = acquisition::make_acquisition(symbol, fs, 50, 10000, 0.008);
+	let acq = acquisition::make_acquisition(symbol, fs, DEFAULT_DOPPLER_STEP_HZ, DEFAULT_DOPPLER_MAX_HZ, test_stat);
 	let trk = tracking::new_default_tracker(prn, acq_freq, fs, DEFAULT_PLL_BW_HZ, DEFAULT_DLL_BW_HZ);
 	let tlm = telemetry_decode::gps::TelemetryDecoder::new();
 
-	Channel{ prn, fs, state, acq, trk, tlm }
+	Channel{ prn, fs, state, acq, trk, tlm, last_acq_doppler:0, last_acq_test_stat: 0.0 }
 }

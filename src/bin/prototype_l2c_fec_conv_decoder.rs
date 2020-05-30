74515f1686b1fb3b858a764646ac3179ea900c75
John Stanford on deathStar <johnwstanford@gmail.com>
2020-05-30 09:04:26 -0500

    Added a binary for prototyping the L2C convolutional FEC decoder

---
+++ src/bin/prototype_l2c_fec_conv_decoder.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+	
+	let symbols:Vec<bool> = vec![false, true, true, true, true, true, true, false, false, false, true, true, true, true, false, true, true, true, true, false, false, true, true, true, true, false, true, false, false, true, false, false, false, false, false, true];
+
+	// Expect 101101111111111010
+
+	println!("{:?}", symbols.len());
+
+}

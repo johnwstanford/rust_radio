35f118f7f0a2d1a38ca985c0bb77ca8321c1b2d8
John Stanford on deathStar <johnwstanford@gmail.com>
2020-06-29 22:39:25 -0500

    Matched filter binary successfully reading in specification file

--- src/bin/matched_filter.rs
+++ src/bin/matched_filter.rs
@@ -1,9 +1,18 @@
 
-extern crate clap;
+use std::fs::File;
+use std::io::BufReader;
 
 use clap::{Arg, App};
+use serde::{Serialize, Deserialize};
 
-fn main() {
+#[derive(Debug, Serialize, Deserialize)]
+struct MatchedFilterSpec {
+	filter: Vec<f64>,
+	filter_sample_rate_sps: Option<f64>,
+	filter_length_sec: Option<f64>
+}
+
+fn main() -> Result<(), &'static str> {
 
 	let matches = App::new("Matched Filter")
 		.version("0.1.0")
@@ -24,5 +33,17 @@ fn main() {
 			.short("j").long("json_spec")
 			.required(true).takes_value(true))
 		.get_matches();
+
+	// Open specification file
+	let spec:MatchedFilterSpec = {
+		let fname:&str = matches.value_of("json_spec").ok_or("No JSON specification file provided")?;
+		let file = File::open(fname).map_err(|_| "Unable to open JSON specification file")?;
+		let reader = BufReader::new(file);
+		serde_json::from_reader(reader).map_err(|_| "Unable to parse JSON specification")?
+	};
+
+	println!("{:?}", spec);
+
+	Ok(())
 
 }
